c6eb7f5ff5a45f2ee286d3749951dec387ef826d
Da <weida_lim@hotmail.com>
2021-04-14 23:52:02 +0800

    Now can get results from youtube

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import Youtube from "../Apis/Youtube";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    console.log(term);
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await Youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+    console.log(this.state.videos);
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have
+        {" " + this.state.videos.length} videos.
       </div>
     );
   }
